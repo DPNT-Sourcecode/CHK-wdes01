addfec49045f86416524fc3b53f9222a4401232c
sbx_user1051 <sbx_user1051@169.254.13.37>
2025-03-29 12:30:01 +0000

    Sat Mar 29 12:30:01 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,19 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-inventory = {"A": 50, "B": 30, "C": 20, "D": 15}
+from collections import defaultdict
+from typing import DefaultDict
+
+
+prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+specials = {"3A": 130, "2B": 25}
 
 
 def checkout(skus):
+    cart = defaultdict(int)
     for sku in skus:
-        if sku not in inventory:
+        if sku not in prices:
             return -1
+        cart[sku] += 1
+
 

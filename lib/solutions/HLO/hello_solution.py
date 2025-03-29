731fe55a0f8a6b9b07ba431082af6b9739f5b527
sbx_user1051 <sbx_user1051@169.254.13.37>
2025-03-29 12:19:35 +0000

    Sat Mar 29 12:19:35 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,5 +1,8 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
+# - {"method":"hello","params":["Craftsman"],"id":"HLO_R1_001"}, expected a value that equals "Hello, World!", got: "Hello, Craftsman!"
+# - {"method":"hello","params":["Mr. X"],"id":"HLO_R1_002"}, expected a value that equals "Hello, World!", got: "Hello, Mr. X!"
 def hello(friend_name):
-    return "Hello, " + friend_name + "!"
+    return "Hello, World!"
+
 

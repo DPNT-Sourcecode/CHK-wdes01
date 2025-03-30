be6136b1f0308fea497c8b1e16f9eebe94f1e4b5
sbx_user1051 <sbx_user1051@169.254.33.135>
2025-03-30 13:32:17 +0000

    Sun Mar 30 13:32:17 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,13 +1,6 @@
+
+
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-# - {"method":"hello","params":["Craftsman"],"id":"HLO_R1_001"}, expected a value that equals "Hello, World!", got: "Hello, Craftsman!"
-# - {"method":"hello","params":["Mr. X"],"id":"HLO_R1_002"}, expected a value that equals "Hello, World!", got: "Hello, Mr. X!"
-# def hello(friend_name):
-#     return "Hello, World!"
-
-# You are given the name of a friend. Say hello to them!
-# Example: if name of friend is "John" than return "Hello, John!".
-
-
 def hello(friend_name):
-    return f"Hello, {friend_name}!"
+    raise NotImplementedError()
